c855e0196824d25a939ae1ad9395d446744f7960
thanidacwn <92778351+thanidacwn@users.noreply.github.com>
2023-09-12 00:00:33 +0700

    delete votes field in choice model

--- polls/models.py
+++ polls/models.py
@@ -58,7 +58,6 @@ class Choice(models.Model):
     """ Choice model """
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
 
     @property
     def votes(self):
